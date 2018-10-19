2cd2efc03320560ee3d0b672bda3bd7b990c3e82
YuliaPisarevich <31861575+YuliaPisarevich@users.noreply.github.com>
2018-10-19 07:34:57 +0300

    Update Test.java

--- 3_task/src/Test.java
+++ 3_task/src/Test.java
@@ -3,10 +3,6 @@ public class Test {
         double r1 = UserInput.nextDouble();
         double r2 = UserInput.nextDouble();
 
-        if (r2 > r1) {
-            r1 = r1 + r2 - (r2 = r1);
-        }
-
         double ringSquare = SquareLogic.SquareRing(r1, r2);
         System.out.println("площадь кольца = " + ringSquare);
     }
